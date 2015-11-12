0951c6eb1e44ea6037bbc633e80dc0fdc674b0e8
GaoMatrix <gcquan08@gmail.com>
2015-11-12 18:28:31 +0800

    1、添加Activity Stack管理Activity 2、添加一些工具类

--- src/com/gao/android/activity/BaseActivity.java
+++ src/com/gao/android/activity/BaseActivity.java
@@ -1,12 +1,31 @@
 package com.gao.android.activity;
 
+import com.gao.android.util.ActivityManagerUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // for subclass
+        initView();
+        // for subclass
+        initData();
+        // add Activity to Activity Stack
+        ActivityManagerUtils.getInstance().addActivity(this);
+    }
+
+    public abstract void initView();
+
+    public abstract void initData();
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // finish this Activity
+        ActivityManagerUtils.getInstance().finishActivity(this);
     }
 }
